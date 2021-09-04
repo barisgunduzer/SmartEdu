26aef95349094043ca09abb6c2eaaccde96d8283
Barış Gündüzer <barisgunduzer@users.noreply.github.com>
2021-09-05 00:38:05 +0300

    bcrypt added

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const pageRouter = require('./routers/pageRouter');
 const courseRouter = require('./routers/courseRouter');
 const categoryRouter = require('./routers/categoryRouter');
+const userRouter = require('./routers/userRouter');
 const app = express();
 
 //Connect DB
@@ -18,10 +19,11 @@ app.use(express.static('public'));
 app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
-//Routes
+//Routers
 app.use('/', pageRouter);
 app.use('/courses', courseRouter);
 app.use('/categories', categoryRouter);
+app.use('/users', userRouter);
 
 //Configs
 const port = 3000;
