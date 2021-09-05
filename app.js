1223e2d7c7c101d26e1868993810122e621f0529
Barış Gündüzer <barisgunduzer@users.noreply.github.com>
2021-09-06 00:28:58 +0300

    User registry,login and sessions added

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const session = require('express-session');
 const pageRouter = require('./routers/pageRouter');
 const courseRouter = require('./routers/courseRouter');
 const categoryRouter = require('./routers/categoryRouter');
@@ -7,17 +8,31 @@ const userRouter = require('./routers/userRouter');
 const app = express();
 
 //Connect DB
-mongoose.connect('mongodb://localhost/smartedu-db').then(() => {
-  console.log('DB connected succesfully!');
-});
+mongoose
+  .connect('mongodb://localhost/smartedu-db', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  })
+  .then(() => {
+    console.log('DB connected succesfully!');
+  });
 
 //Template Engine
 app.set('view engine', 'ejs');
 
 //Middlewares
 app.use(express.static('public'));
-app.use(express.json()) // for parsing application/json
-app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use(
+  session({
+    secret: 'my_keyboard_cat',
+    resave: false,
+    saveUninitialized: true,
+  })
+);
 
 //Routers
 app.use('/', pageRouter);
@@ -28,6 +43,6 @@ app.use('/users', userRouter);
 //Configs
 const port = 3000;
 
-app.listen(port, () => {  
+app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
 });
